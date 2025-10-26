f18b4d1b4facd26cc9e4fdc62f9adc78fc1b8a51
xujinyang <1441739534@qq.com>
2025-10-26 16:56:24 +0800

    员工查询代码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -29,7 +29,11 @@ public interface EmployeeService {
 
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
-
-
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    void starOrStop(Integer status, Long id);
 
 }
