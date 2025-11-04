ee0ac46bc006435f623c99f90b4d2bd36ae78963
xujinyang <1441739534@qq.com>
2025-11-04 15:54:07 +0800

    公共字段填充功能代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -89,13 +89,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(DEFAULT_PASSWORD.getBytes()));
 
         //设置时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
+       // employee.setCreateTime(LocalDateTime.now());
+       // employee.setUpdateTime(LocalDateTime.now());
 
         //TODO 后续修改当前登录用户id
         //设置用户id
-        employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+       // employee.setCreateUser(BaseContext.getCurrentId());
+        //employee.setUpdateUser(BaseContext.getCurrentId());
 
 
         //实体层用mapper
@@ -165,8 +165,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         //这个地方是DTO，需要转换一下
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+    //    employee.setUpdateTime(LocalDateTime.now());
+    //    employee.setUpdateUser(BaseContext.getCurrentId());
         employeeMapper.update(employee);
 
     }
