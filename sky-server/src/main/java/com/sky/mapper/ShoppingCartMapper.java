cbff56706d386569ef5446df8c54e20155bcbf03
xujinyang <1441739534@qq.com>
2026-01-03 21:00:11 +0800

    购物车功能模块

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,28 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+    /**
+     * 获取购物车数据
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 更新数据
+     * @param cart
+     */
+    void updateNumberById(ShoppingCart cart);
+
+    /**
+     * 插入新的购物数据
+     * @param shoppingCart
+     */
+    void insert(ShoppingCart shoppingCart);
+}
